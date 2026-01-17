c73a9f026dc8c8b7b1b738bef1f11b8478057ed9
sevetom <sevetom2611@libero.it>
2026-01-17 18:17:43 +0100

    feat: not found page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,31 +1,18 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import ProfileView from './pages/ProfileView.vue'
+import AppHeader from './components/AppHeader.vue';
+import MainStructure from './components/MainStructure.vue';
 </script>
 
 <template>
-  <!-- <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div> -->
-  <ProfileView />
+  <div class="min-h-screen bg-whitesmoke-200 flex flex-col p-2.5 gap-2.5">
+    <AppHeader />
+    <MainStructure>
+      <template #left>
+        <RouterView name="left" />
+      </template>
+      <template #right>
+        <RouterView name="right" />
+      </template>
+    </MainStructure>
+  </div>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
---
+++ frontend/src/pages/LogoPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div
+    class="h-full flex-1 w-full relative rounded-tl-[10px] rounded-tr-none rounded-br-none rounded-bl-[10px] bg-gray-100 overflow-hidden shrink-0 flex flex-col items-center justify-center py-[1.875rem] px-[1.25rem] box-border text-left text-[3rem] text-darkslategray font-inter"
+  >
+    <div
+      class="w-[35rem] h-[31.625rem] overflow-hidden shrink-0 flex flex-col items-center justify-center gap-[0.187rem]"
+    >
+      <div
+        class="self-stretch overflow-hidden flex items-center justify-center gap-[0.937rem]"
+      >
+        <img src="../assets/logo.png" class="w-[3.75rem] relative max-h-full object-cover" alt="" />
+        <div class="relative font-semibold">Easy Notes</div>
+      </div>
+      <div
+        class="self-stretch overflow-hidden flex items-center justify-center text-[1.875rem] text-gray-200"
+      >
+        <div class="relative font-semibold">Note taking made easy</div>
+      </div>
+    </div>
+  </div>
+</template>
