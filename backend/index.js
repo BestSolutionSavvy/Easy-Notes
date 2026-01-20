d1cecab1d17e0317fc1f6016d8e0dcd4717d6b89
sevetom <sevetom2611@libero.it>
2026-01-20 18:29:50 +0100

    feat: integrated swagger

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,12 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const movieRouter = require('./src/routes/notebookRoutes');
 const cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./api.yaml');
+
+const notebookRouter = require('./src/routes/notebookRoutes');
 
 mongoose.connect(process.env.MONGO_URI);
 
@@ -12,7 +16,9 @@ app.use(express.json());
 app.use(express.static('public'));
 
 app.use('/notebooks', notebookRouter);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(3000, () => {
-    console.log('Server listening on port 3000');
+    console.log('Server listening on http://localhost:3000');
+    console.log('API docs available at http://localhost:3000/api-docs');
 });
