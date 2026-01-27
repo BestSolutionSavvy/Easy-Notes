fa9759657a64417bc970284a4d02c984ae50da30
sevetom <sevetom2611@libero.it>
2026-01-27 18:15:43 +0100

    feat: pdf different color for easy recognition

--- frontend/src/components/AddItemButton.vue
+++ frontend/src/components/AddItemButton.vue
@@ -2,10 +2,13 @@
 interface Props {
   icon: string;
   alt?: string;
+  background?: string;
   onClick: () => void;
 }
 
-const props = defineProps<Props>();
+const props = withDefaults(defineProps<Props>(), {
+  background: 'bg-gradient-to-r from-indigo-700 to-blue-950'
+});
 
 const handleClick = () => {
   props.onClick();
@@ -15,9 +18,9 @@ const handleClick = () => {
 <template>
   <div
     @click="handleClick"
-    class="w-6 h-6 bg-gradient-to-r from-indigo-700 to-blue-950 rounded-[32px] inline-flex 
+    :class="`w-6 h-6 ${props.background} rounded-[32px] inline-flex 
       justify-center items-center gap-2 cursor-pointer hover:opacity-90 transition-opacity
-      ease-in-out hover:scale-[1.02] active:scale-[0.98] transition-all duration-200"
+      ease-in-out hover:scale-[1.02] active:scale-[0.98] transition-all duration-200`"
   >
     <div class="flex justify-center items-center">
       <img class="w-3 h-3" :alt="props.alt || ''" :src="props.icon" />
